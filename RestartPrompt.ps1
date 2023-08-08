390c3e083f219e9059a00270f9938f3a90a4b50b
shawn <shawntan2@gmail.com>
2023-08-08 09:08:19 +0800

    Add files via upload

---
+++ RestartPrompt.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+Function RestartFunction{
+shutdown /r /f /t 5
+}
+
+Function PromptBox{
+$boxInput=[System.Windows.MessageBox]::Show("Windows has just updated and needs to restart to complete the update. Remember to save your work if you have not done so. Click Yes to restart now or No to delay the restart for 10 minutes.","Restart?","YesNo","question")
+OnClickRun
+}
+
+function OnClickRun{
+
+switch($boxInput){
+"Yes"{
+Write-Host "input is yes"
+RestartFunction
+     }
+"No"{
+Write-Host "input is no"
+Start-Sleep -Seconds 600
+PromptBox
+    }
+	}
+           }
+
+PromptBox
