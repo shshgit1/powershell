26a73e180c9fb5f7481914f4511a7174c1b3d67f
shawn <shawntan2@gmail.com>
2024-04-05 15:26:32 +0800

    Update DurationSinceRestart.ps1

--- DurationSinceRestart.ps1
+++ DurationSinceRestart.ps1
@@ -8,9 +8,9 @@ $timeDifference=New-TimeSpan -Start $(gcim win32_operatingsystem).lastbootuptime
 $timeDifferenceInDays=$timeDifference.days
 
 if ($timeDifferenceInDays -gt 6){
-echo "$timeDifferenceInDays days since restart. Current date and time is $today. Current user is $env:USERNAME">> N:\ICT\scripts\updaterestart\DevicesRequiringRestart\$env:COMPUTERNAME.txt
+echo "$timeDifferenceInDays days since restart. Current date and time is $today. Current user is $env:USERNAME">> #log file location
 # $boxInput=[System.Windows.MessageBox]::Show("Windows has installed an update. To complete the update, please restart your computer. Should you have any questions, please do not hesitate to contact ICT support.")
 }
 else{
-echo "$timeDifferenceInDays days since restart. Current date and time is $today. Current user is $env:USERNAME. This devices does not require a restart.">> N:\ICT\scripts\updaterestart\DevicesRequiringRestart\$env:COMPUTERNAME.txt
+echo "$timeDifferenceInDays days since restart. Current date and time is $today. Current user is $env:USERNAME. This devices does not require a restart.">> #log file location
 }
